55e52e6013d2ebc1502459dfc19936dd1bf3ad3b
Khan Atik Faisal <81611894+khanatik1176@users.noreply.github.com>
2023-10-18 09:18:45 +0600

    Conditional.asm

--- 21-44602-1_COA_labwork/Conditional.asm
+++ 21-44602-1_COA_labwork/Conditional.asm
@@ -1,3 +1,70 @@
 .model small
 
-.stack 100h;
+.stack 100h
+
+.data
+
+Hello db "Hello World$";
+Bye db "Bye World$";
+count1 db 10;
+count2 db 5;
+
+.code
+
+    main proc
+            
+            
+        mov ax,@data;
+        mov ds,ax;
+        
+            
+        mov cx, 10;  counter1 stored in cx
+        mov bx, 5;   counter2 stored in bx
+            
+        ;printing hello world
+        print_hello:
+        lea dx,Hello;
+        mov ah,09h;
+        int  21h;
+        
+        mov ah,02h;
+        mov dl, 0dh;
+        int 21h;
+        mov dl,0ah;
+        int 21h;
+        
+        ;decrement the counter1
+        dec cx;
+        
+        ;Check if we've printed "Hello world 10 times"
+        
+        cmp cx,0;
+        JG print_hello;
+        
+        
+                      
+
+        print_bye:              
+        
+        mov ah,02h;
+        mov dl, 0dh;
+        int 21h;
+        mov dl,0ah;
+        int 21h;
+        
+        
+        lea dx,Bye;
+        mov ah,09h;
+        int 21h;
+          
+        dec bx; decrement couter2
+        cmp bx,0; comparing if bye prints 5 five times or not
+        JG print_bye;
+        
+        ;Return DOS
+        mov ah,4ch;
+        int 21h; DOS Exit
+        
+       
+        main endp;
+    end;
